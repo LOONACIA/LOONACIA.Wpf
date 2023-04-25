dfee41f040aee39206c1c52f785e71061e7856cb
LOONACIA <0041lntc@gmail.com>
2023-04-25 20:54:13 +0900

    Add new properties to PlaceholderTextbox.

--- src/LOONACIA.Wpf/Controls/PlaceholderBox/PlaceholderTextBox.cs
+++ src/LOONACIA.Wpf/Controls/PlaceholderBox/PlaceholderTextBox.cs
@@ -1,11 +1,18 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace LOONACIA.Wpf.Controls;
 public class PlaceholderTextBox : TextBox
 {
 	public static readonly DependencyProperty PlaceholderProperty =
 		DependencyProperty.Register(nameof(Placeholder), typeof(string), typeof(PlaceholderTextBox), new PropertyMetadata(string.Empty));
+
+	public static readonly DependencyProperty PlaceholderBrushProperty =
+		DependencyProperty.Register(nameof(PlaceholderBrush), typeof(Brush), typeof(PlaceholderTextBox), new PropertyMetadata(null));
+
+	public static readonly DependencyProperty CornerRadiusProperty =
+		DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(PlaceholderTextBox), new PropertyMetadata(default));
 
 	static PlaceholderTextBox()
 	{
@@ -16,5 +23,17 @@ public class PlaceholderTextBox : TextBox
 	{
 		get { return (string)GetValue(PlaceholderProperty); }
 		set { SetValue(PlaceholderProperty, value); }
+	}
+
+	public Brush PlaceholderBrush
+	{
+		get { return (Brush)GetValue(PlaceholderBrushProperty); }
+		set { SetValue(PlaceholderBrushProperty, value); }
+	}
+
+	public CornerRadius CornerRadius
+	{
+		get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+		set { SetValue(CornerRadiusProperty, value); }
 	}
 }
