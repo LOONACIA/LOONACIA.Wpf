c31481fce27b0acb39cf9d64fc0ea762e7fe2b1d
LOONACIA <0041lntc@gmail.com>
2023-04-25 20:59:27 +0900

    Add NullToVisibilityConverter.

---
+++ src/LOONACIA.Wpf/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LOONACIA.Wpf.Converters;
+internal class NullToVisibilityConverter : IValueConverter
+{
+	/// <summary>
+	/// Gets or sets a converter is inverted. If true, return <see cref="Visibility.Visible"/> when value is null. otherwise, return <see cref="Visibility.Visible"/> when value is not null.
+	/// </summary>
+	public bool IsInverted { get; set; }
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (IsInverted)
+		{
+			return value is null ? Visibility.Visible : Visibility.Collapsed;
+		}
+		else
+		{
+			return value is not null ? Visibility.Visible : Visibility.Collapsed;
+		}
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return IsInverted ? value is Visibility.Visible : value is not Visibility.Visible;
+	}
+}
