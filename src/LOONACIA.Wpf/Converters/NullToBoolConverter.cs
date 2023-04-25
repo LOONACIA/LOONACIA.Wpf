23c0a5e1299e64ae47fe6705e93190e987259f46
LOONACIA <0041lntc@gmail.com>
2023-04-25 20:53:40 +0900

    Add NullToBoolConverter

---
+++ src/LOONACIA.Wpf/Converters/NullToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LOONACIA.Wpf.Converters;
+public class NullToBoolConverter : IValueConverter
+{
+	/// <summary>
+	/// Gets or sets a converter is inverted. If true, return value is null. otherwise, return value is not null.
+	/// </summary>
+	public bool IsInverted { get; set; }
+
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return IsInverted ? value is null : value is not null;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
